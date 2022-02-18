ae41498507264ca76f9d1a00453831bae897a483
Jonathan Swena <56330128+JonathanSwena@users.noreply.github.com>
2022-02-18 17:13:50 -0600

    Update push.py

--- push.py
+++ push.py
@@ -3,7 +3,7 @@ import sys
 
 
 
-os.system("git add lamps.txt")
-os.system('git commit -m "First Attempt"')
+os.system("git add displayCase.txt")
+os.system('git commit -m "Standard Update"')
 os.system("git push")
 os.system("cd ~/")
